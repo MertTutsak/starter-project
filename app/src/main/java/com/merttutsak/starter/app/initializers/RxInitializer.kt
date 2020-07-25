8b7c96942c7d41263d0d7dcf3e3330492627584e
mert tutsak <merttutsak@gmail.com>
2020-07-25 03:13:27 +0300

    RxInitializer add

---
+++ app/src/main/java/com/merttutsak/starter/app/initializers/RxInitializer.kt
@@ -0,0 +1,14 @@
+package com.merttutsak.starter.app.initializers
+
+import android.app.Application
+import com.orhanobut.logger.Logger
+import io.reactivex.plugins.RxJavaPlugins
+import javax.inject.Inject
+
+class RxInitializer @Inject constructor() : AppInitializer {
+    override fun init(application: Application) {
+        RxJavaPlugins.setErrorHandler {
+            Logger.e(it.localizedMessage)
+        }
+    }
+}
